0e81a574be499bbf1e799bdf1d838d70b668f350
akaneyama <daffapurwanto234@gmail.com>
2024-05-12 17:54:04 +0700

    update

--- youtube-converter.sh
+++ youtube-converter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "masukkan link Youtube: " linkyt
 read -p "Masukkan Judul: " judul
-yt-dlp -f bestaudio --extract-audio --audio-format mp3 --prefer-ffmpeg "$linkyt" -o "./$judul"
+yt-dlp -f bestaudio --extract-audio --audio-format mp3 --prefer-ffmpeg "$linkyt" -o "./%(title)s.%(ext)s"
  
 #for batch
 #yt-dlp -f bestaudio --extract-audio --audio-format mp3 --prefer-ffmpeg -a "$linkyt" -o "./$judul"
